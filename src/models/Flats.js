18e2b32f627ad8c0e39cbec84dc47999f9de6e82
Sazzad Hossain <shadshanto@gmail.com>
2025-01-04 19:09:06 +0600

    Feature: Fetching and displaying the flats in homepage.Take multiple image input.

--- src/models/Flats.js
+++ src/models/Flats.js
@@ -1,6 +1,5 @@
-// models/Flats.js
 import mongoose from "mongoose";
-
+import Property from "./Properties.js";
 const flatSchema = new mongoose.Schema({
     property_id: { type: Number, required: true },
     flat_number: { type: String, required: true },
@@ -16,12 +15,23 @@ const flatSchema = new mongoose.Schema({
 const Flat = mongoose.model("Flat", flatSchema);
 
 export const FlatModel = {
+    async getVacantFlats(propertyIds) {
+        return await Flat.find({
+            property_id: { $in: propertyIds },
+            status: 'vacant'
+        });
+    },
     async getFlatsForProperty(propertyId) {
         return await Flat.find({ property_id: propertyId });
     },
     
     async addFlat(flatData) {
         const newFlat = new Flat(flatData);
+        await Property.findOneAndUpdate(
+            { p_id: flatData.property_id },
+            { $inc: { total_flats: 1 } },
+            { new: true }
+        );
         return await newFlat.save();
     }
 };
